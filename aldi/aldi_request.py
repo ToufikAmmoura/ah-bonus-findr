825f69cc898104c53e4ba579541472ebc663a8d5
Toufik Ammoura <toufikammoura@gmail.com>
2023-12-13 22:48:13 +0100

    doing work

--- aldi/aldi_request.py
+++ aldi/aldi_request.py
@@ -1,3 +1,5 @@
+import asyncio
+import aiohttp
 import requests
 from lxml import html
 
@@ -9,19 +11,31 @@ with open('aldi.html', 'w') as file:
 tree = html.fromstring(response.content)
 elements = tree.xpath('//div[contains(@data-tile-url, "") and contains(@class, "article")]/@data-tile-url')
 
+article_urls = []
+results = []
 article_titles = []
 
 for el in elements:
   base_url = 'https://www.aldi.nl'
   article_url = base_url + el
-  article_response = requests.get(article_url)
-  tree = html.fromstring(article_response.content)
-  article_title = tree.xpath('//span[@class="mod-article-tile__title"]')[0]
-  article_titles.append(article_title.text.strip())
+  article_urls.append(article_url)
 
-for article_title in article_titles:
-  print(article_title)
+async def fetch_url(url):
+  async with aiohttp.ClientSession() as session:
+    async with session.get(url) as response:
+      return await response.text()
+    
+async def main():
+  tasks = [fetch_url(url) for url in article_urls]
+  results = await asyncio.gather(*tasks)
+  return results
 
 
-# mogelijke xpath: //div[contains(@data-tile-url, "") and contains(@class, "article")]/@data-tile-url
-# xpath titel: //span[@class="mod-article-tile__title"]
+if __name__ == '__main__':
+  results = asyncio.run(main())
+
+  for result in results:
+    tree = html.fromstring(result)
+    article_title = tree.xpath('//span[@class="mod-article-tile__title"]')[0]
+    article_titles.append(article_title.text.strip())    
+    print(article_title.text.strip())
