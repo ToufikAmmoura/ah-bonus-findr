76196eb9074e0d5a6cd40a9afcd8716274b14f31
Toufik Ammoura <toufikammoura@gmail.com>
2024-02-07 20:56:58 +0100

    refactor

--- albert_heijn/ah_request.py
+++ albert_heijn/ah_request.py
@@ -5,15 +5,12 @@ from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
 options = Options()
-options.add_argument('--headless=new')
-options.add_argument('user-agent=Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36')
+options.add_argument('user-agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36')
 driver = webdriver.Chrome(options=options)
-driver.get('https://www.jumbo.com/aanbiedingen/')
+driver.get('https://www.ah.nl/bonus')
 
-
-driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
 wait = WebDriverWait(driver, 10)
-elements = wait.until(EC.presence_of_all_elements_located((By.XPATH, '//h3/a')))
+elements = wait.until(EC.presence_of_all_elements_located((By.XPATH, '//p[contains(@class, "card-title")]')))
 
 for el in elements:
   print(el.text)
